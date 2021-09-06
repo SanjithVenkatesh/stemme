f21eb4f8c36af5fe5b9c5aba02d18f0fc15a6b8f
Sanjith Venkatesh <sanjith@pacbell.net>
2021-09-05 19:52:55 -0700

    Candidate and Party data structures added and necessary modifications to the unit test

--- mmp.py
+++ mmp.py
@@ -1,4 +1,27 @@
-import operator
+from typing import List
 
-def mmp(votes, parties, seats, combined_party_lists, constituency_votes):
-    return []
+class Candidate:
+    def __init__(self, name: str, party: str):
+        self.name = name
+        self.party = party
+
+class Party:
+    def __init__(self, names: list[str], party_name: str):
+        self.party_members = names
+        self.party_name = party_name
+    
+    def generate_candidates(self):
+        candidates: List[Candidate] = []
+        for member in self.party_members:
+            candidates.append(Candidate(member, self.party_name))
+        return candidates
+    
+    def generate_candidate(self, candidate_name):
+        if candidate_name not in self.party_members:
+            raise Exception("candidate not in party")
+        return Candidate(candidate_name, self.party_name)
+
+def mmp(votes, parties, seats, constituency_votes):
+    winners = []
+
+    return winners
--- test_mmp.py
+++ test_mmp.py
@@ -1,21 +1,9 @@
-from mmp import mmp
+from mmp import mmp, Candidate, Party
 
 
 def test_mmp():
     # Assembly foundations
     seats = 10
-    # Registered Parties
-    parties = ["SPD", "Union", "Grune", "Linke", "AFD"]
-
-    # Party Vote
-    party_vote = {"SPD": 250, "Union": 550, "Grune": 150, "Linke": 40, "AFD": 90}
-
-    # Five constituencies
-    berlin = {"SPD": 40, "Grune": 30, "Linke": 35, "Union": 20, "AFD": 5} # SPD winner
-    saxony = {"SPD": 40, "Grune": 30, "Linke": 5, "Union": 50, "AFD": 90} # AFD winnder
-    bavaria = {"SPD": 30, "Grune": 15, "Linke": 5, "Union": 100, "AFD": 20} # Union winner
-    frankfurt = {"SPD": 25, "Grune": 45, "Linke": 15, "Union": 40, "AFD": 5} # Grune winner
-    baden = {"SPD": 20, "Grune": 60, "Linke": 5, "Union": 40, "AFD": 15} # Grune winner
 
     # Party lists
     spd_list = create_list("spd", seats)
@@ -23,13 +11,42 @@ def test_mmp():
     grune_list = create_list("g", seats)
     afd_list = create_list("afd", seats)
     linke_list = create_list("linke", seats)
+    print(spd_list)
 
-    combined_party_lists = [spd_list, union_list, grune_list, afd_list, linke_list]
+    SPD = Party(spd_list, "SPD")
+    Grune = Party(grune_list, "Grune")
+    Union = Party(union_list, "Union")
+    AFD = Party(afd_list, "AFD")
+    Linke = Party(linke_list, "Linke")
+
+    # Party Vote
+    party_vote = {SPD: 250, Union: 550, Grune: 150, Linke: 40, AFD: 90}
+
+    # Registered Parties
+    parties = [SPD, Grune, Union, AFD, Linke]
+
+    # Candidate Lists
+    spd_candidates = SPD.generate_candidates()
+    union_candidates = Union.generate_candidates()
+    grune_candidates = Grune.generate_candidates()
+    afd_candidates = AFD.generate_candidates()
+    linke_candidates = Linke.generate_candidates()
+    print(spd_candidates)
+
+    # Five constituencies
+    berlin = { spd_candidates[0]: 40, grune_candidates[0]: 30, linke_candidates[0]: 35, union_candidates[0]: 20, afd_candidates[0]: 5} # SPD winner
+    saxony = {spd_candidates[1]: 40, grune_candidates[1]: 30, linke_candidates[1]: 5, union_candidates[1]: 50, afd_candidates[1]: 90} # AFD winnder
+    bavaria = {spd_candidates[2]: 30, grune_candidates[2]: 15, linke_candidates[2]: 5, union_candidates[2]: 100, afd_candidates[2]: 20} # Union winner
+    frankfurt = {spd_candidates[3]: 25, grune_candidates[3]: 45, linke_candidates[3]: 15, union_candidates[3]: 40, afd_candidates[3]: 5} # Grune winner
+    baden = {spd_candidates[4]: 20, grune_candidates[4]: 60, linke_candidates[4]: 5, union_candidates[4]: 40, afd_candidates[4]: 15} # Grune winner
+
     constituency_votes = [berlin, saxony, bavaria, frankfurt, baden]
 
     candidates_elected = ["spd1", "spd2", "u1", "u2", "u3", "u4", "u5", "g1", "g2", "afd1"]
 
-    assert mmp(party_vote, parties, 10, combined_party_lists, constituency_votes) == candidates_elected
+    mmp_winners = mmp(party_vote, parties, 10, constituency_votes)
+
+    assert mmp_winners == candidates_elected
 
 def create_list(party_slug, seats):
     candidates = []
