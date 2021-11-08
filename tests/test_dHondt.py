032c3fe97ebe92792eb442538fde8b67cca52a9e
Sanjith Venkatesh <sanjith@pacbell.net>
2021-11-07 23:22:32 -0800

    Refactored stv into proper class

--- tests/test_dHondt.py
+++ tests/test_dHondt.py
@@ -3,27 +3,32 @@ import pytest
 from errors import InvalidCSVFileError
 from methods.base import Party, create_list
 
-seats = 8
+seats: int = 8
+
 
 @pytest.fixture
 def A():
-    return Party(names=create_list('a', seats), party_name='A', votes=100)
+    return Party(names=create_list("a", seats), party_name="A", votes=100)
+
 
 @pytest.fixture
 def B():
-    return Party(names=create_list('b', seats), party_name='B', votes=80)
+    return Party(names=create_list("b", seats), party_name="B", votes=80)
+
 
 @pytest.fixture
 def C():
-    return Party(names=create_list('c', seats), party_name='C', votes=30)
+    return Party(names=create_list("c", seats), party_name="C", votes=30)
+
 
 @pytest.fixture
 def D():
-    return Party(names=create_list('d', seats), party_name='D', votes=20)
+    return Party(names=create_list("d", seats), party_name="D", votes=20)
+
 
 @pytest.fixture
 def parties(A, B, C, D):
-    return [A,B,C,D]
+    return [A, B, C, D]
 
 
 def test_dHondt_with_list_candidates(parties):
@@ -31,13 +36,15 @@ def test_dHondt_with_list_candidates(parties):
     winning_candidates = dh.calculate_winners(candidates=True)
     print(winning_candidates)
 
-    assert winning_candidates == ['a1', 'b1', 'a2', 'b2', 'a3', 'c1', 'b3', 'a4']
+    assert winning_candidates == ["a1", "b1", "a2", "b2", "a3", "c1", "b3", "a4"]
+
 
 def test_dHondt_with_dict_party_seats(parties, A, B, C, D):
     dh = dHondt(name="dh", seats=8, input=parties)
     winning_candidates = dh.calculate_winners(party_seats=True)
 
     assert winning_candidates == {A: 4, B: 3, C: 1, D: 0}
+
 
 def test_dHondt_with_csv(A, B, C, D):
     party_votes = "dHondt_csv/party_votes.csv"
@@ -57,7 +64,5 @@ def test_throw_invalidcsvfileerror():
 def test_dHondt_gallagher_index(parties):
     dh = dHondt(name="dh", seats=8, input=parties)
     _ = dh.calculate_winners(party_seats=True)
-    
-    assert dh.gallagher_index() == 7.93
 
-    
+    assert dh.gallagher_index() == 7.93
--- tests/test_gallagher.py
+++ tests/test_gallagher.py
@@ -4,31 +4,45 @@ import pytest
 
 seats = 8
 
+
 @pytest.fixture
 def Liberal():
-    return Party(names=create_list('l', seats), party_name='Liberal', votes=100)
+    return Party(names=create_list("l", seats), party_name="Liberal", votes=100)
+
 
 @pytest.fixture
 def Conservative():
-    return Party(names=create_list('c', seats), party_name='Conservative', votes=80)
+    return Party(names=create_list("c", seats), party_name="Conservative", votes=80)
+
 
 @pytest.fixture
 def NDP():
-    return Party(names=create_list('n', seats), party_name='NDP', votes=30)
+    return Party(names=create_list("n", seats), party_name="NDP", votes=30)
+
 
 @pytest.fixture
 def BQ():
-    return Party(names=create_list('bq', seats), party_name='BQ', votes=20)
+    return Party(names=create_list("bq", seats), party_name="BQ", votes=20)
+
 
 @pytest.fixture
 def Greens():
-    return Party(names=create_list('g', seats), party_name='Greens', votes=20)
+    return Party(names=create_list("g", seats), party_name="Greens", votes=20)
+
 
 @pytest.fixture
 def Other():
-    return Party(names=create_list('o', seats), party_name='Other', votes=20)
+    return Party(names=create_list("o", seats), party_name="Other", votes=20)
+
 
 def test_calculate_gallager_index(Liberal, Conservative, NDP, BQ, Greens, Other):
-    party_stats = {Liberal: (184, 3947), Conservative: (99, 3189), NDP: (44, 1971), BQ: (10, 466), Greens: (1, 345), Other: (0, 82)}
+    party_stats = {
+        Liberal: (184, 3947),
+        Conservative: (99, 3189),
+        NDP: (44, 1971),
+        BQ: (10, 466),
+        Greens: (1, 345),
+        Other: (0, 82),
+    }
 
     assert Election.calculate_gallagher_index(party_stats=party_stats) == 12.02
